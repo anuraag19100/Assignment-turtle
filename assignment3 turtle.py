f8f13112c2289b368f54295eb90a996d2c8e054d
anuraag19100 <52529329+anuraag19100@users.noreply.github.com>
2019-07-13 14:43:27 +0530

    Add files via upload

---
+++ assignment3 turtle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import turtle 
+
+turtle.color("green")
+turtle.begin_fill()
+turtle.circle(130,180)
+turtle.end_fill()
+turtle.hideturtle()
+turtle.color("orange")
+turtle.begin_fill()
+turtle.circle(130,180)
+turtle.end_fill()
+turtle.hideturtle()
+
+
+# In[ ]:
+
+
+#Draw a Spirling Square with Pen color as 'Red'
+import turtle as y
+a=y.Turtle()
+a.pencolor('red')
+for i in range (100):
+    a.forward(i)
+    a.left(91)
+y.done()
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
